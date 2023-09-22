011be9cdb3cca060f79da62f4598bfc15dd767ea
baolinh <107604287+baolinh-dev@users.noreply.github.com>
2023-09-22 16:26:59 +0700

    Delete the test project and quickly build the reactjs project with cra

--- setup-reactjs-project/src/App.tsx
+++ setup-reactjs-project/src/App.tsx
@@ -1,9 +1,22 @@
+import { useState } from 'react'
+
 function App() {
+  const [count, setCount] = useState(0)
+  const [prevCount, setPrevCount] = useState(0)
+
+  const handleIncrease = () => {
+    setCount((prevCount) => {
+      // đoạn này setPrevCount để có thể xem giá trị trước đó
+      setPrevCount(prevCount)
+      return prevCount + 1
+    })
+  }
+
   return (
-    <div className='App'>
-      <a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
-        Learn React
-      </a>
+    <div>
+      <p>count: {count}</p>
+      <p>preCount: {prevCount}</p>
+      <button onClick={handleIncrease}>Increase</button>
     </div>
   )
 }
