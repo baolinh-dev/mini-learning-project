d771d480e9e5825ffa48d65005a23af59a5f6fca
baolinh <107604287+baolinh-dev@users.noreply.github.com>
2023-09-25 20:17:42 +0700

    learn useEffect with no dependencies

--- setup-reactjs-project/src/App.tsx
+++ setup-reactjs-project/src/App.tsx
@@ -1,15 +1,22 @@
-import { useState, ChangeEvent} from 'react'
+import { useState, ChangeEvent, useEffect } from 'react'
 
 function App() {
   const [todo, setTodo] = useState('')
   const [todoList, setTodoList] = useState<string[]>([])
 
+  // Khi component re-render thì callback sẽ được gọi
+  useEffect(() => {
+    document.title = todo
+  })
+
   function handleChangeTodo(e: ChangeEvent<HTMLInputElement>) {
     setTodo(e.target.value)
-    console.log('todo', todo)
+    console.log('todo:', todo)
   }
+
   function handleSaveTodo() {
     setTodoList((prevTodoList) => [...prevTodoList, todo])
+    console.log('todoList:', todoList)
   }
 
   // đoạn này là giao diênhjtức
